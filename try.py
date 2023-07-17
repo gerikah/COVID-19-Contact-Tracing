394a643c7eb077aabc54bf2ee081c1aacd02f7f6
gerikah <aldaygerikah@gmail.com>
2023-07-17 11:36:31 +0800

    final project instructions

---
+++ try.py
@@ -0,0 +1,13 @@
+# For your final project:
+
+# 1. Create “your” own covid contact tracing app with GUI
+#	- Create a new repository in github
+#	- Create a program that ask user for typical information found in covid contact tracing app
+#	- Write the collected information in a file (use any format you like)
+#	- The app should be able to add and search entry
+#	- Be creative, the realistic the better.
+#	- Please don't submit downloaded program
+#	- Please don’t follow any online contact tracing programming tutorial
+#	- Your program should be uploaded to github before July 29, the earlier the better
+#	- Record a demo presenting your contact tracing app (less than 3 min only)
+#	- Send the demo to my messenger before July 29, the earlier the better
