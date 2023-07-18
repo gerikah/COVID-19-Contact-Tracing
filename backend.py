35bf2c16bb8ca5a02d10cca81ce1d12d86e3d730
gerikah <aldaygerikah@gmail.com>
2023-07-18 16:25:08 +0800

    read file and save to all contacts

--- backend.py
+++ backend.py
@@ -7,4 +7,7 @@ class COVID_Contacts_Informations:
 
     def contacts(self): # to load the contacts saved in csv
         try:
-            with open("covid_contacts_record.csv") as file:
+            with open("covid_contacts_record.csv", mode="r") as file:
+                reader = csv.reader(file) # set read mode using csv reader
+                self.all_contacts = list(reader) # save sa all_contacts
+        
