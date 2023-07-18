b3a3d2e7fc40ed50568dfe35a5a3ae4f15c9a367
gerikah <aldaygerikah@gmail.com>
2023-07-18 16:22:38 +0800

    open the csv file, import file

--- backend.py
+++ backend.py
@@ -1,7 +1,10 @@
+import csv
 
 # create the class
 class COVID_Contacts_Informations:
     def __init__(self):
         self.all_contacts = []
 
-    def contacts(self): # for the contacts saved in csv
+    def contacts(self): # to load the contacts saved in csv
+        try:
+            with open("covid_contacts_record.csv") as file:
