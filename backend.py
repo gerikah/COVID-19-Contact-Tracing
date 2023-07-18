5dcd5cd5bae2430850eae1bd4773f980ee2ae5d7
gerikah <aldaygerikah@gmail.com>
2023-07-18 16:28:13 +0800

    exception handling if file is not found

--- backend.py
+++ backend.py
@@ -10,4 +10,8 @@ class COVID_Contacts_Informations:
             with open("covid_contacts_record.csv", mode="r") as file:
                 reader = csv.reader(file) # set read mode using csv reader
                 self.all_contacts = list(reader) # save sa all_contacts
+        except FileNotFoundError: # exception handling if file is not found
+            pass
+        except Exception as e: # error message
+            raise Exception(f"Failed to load address book: {str(e)}")
         
