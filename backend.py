fc5e6b35b44988fe549e9c6685d1617e7e92cdd4
gerikah <aldaygerikah@gmail.com>
2023-07-18 16:18:14 +0800

    def for loading contacts in file

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 
 # create the class
 class COVID_Contacts_Informations:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.all_contacts = []
+
+    def contacts(self): # for the contacts saved in csv
