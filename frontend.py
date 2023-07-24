fdf8e0c4b8a03f118b3554c1fe1202e6c94e66fa
gerikah <aldaygerikah@gmail.com>
2023-07-25 01:21:37 +0800

    imports - tkinter and class from other file

---
+++ frontend.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+from tkinter import ttk
+from tkinter import filedialog
+import tkinter.font as tkfont
+from backend import COVID_Contacts_Informations
+
--- try.py
+++
@@ -1,9 +0,0 @@
-# For your final project:
-
-# Create “your” own covid contact tracing app with GUI
-    # need mag import ng tkinter
-# Create a program that ask user for typical information found in covid contact tracing app
-    # name, phone num, address
-# Write the collected information in a file (use any format you like)
-    # add yung info sa csv file
-# The app should be able to add and search entry
