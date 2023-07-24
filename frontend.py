e83e87e7afba86e0b75255b86f0e2bca51e1183a
gerikah <aldaygerikah@gmail.com>
2023-07-25 01:24:58 +0800

    class

--- frontend.py
+++ frontend.py
@@ -5,3 +5,10 @@ from tkinter import filedialog
 import tkinter.font as tkfont
 from backend import COVID_Contacts_Informations
 
+class COVID_Contacts_Information_GUI:
+    def __init__(self) -> None:
+        
+        
+root = tk.Tk()
+gui = COVID_Contacts_Information_GUI(root)
+root.mainloop()
