cbbcaf551f3e087f13a433fc6d8e643bd239e333
gerikah <aldaygerikah@gmail.com>
2023-07-27 14:34:53 +0800

    back to previous code before mag ka error

--- frontend.py
+++ frontend.py
@@ -7,6 +7,7 @@ import tkinter.font as tkfont
 from PIL import Image, ImageTk
 import csv
 
+
 class COVID_Contacts_Information:
     def __init__(self, master):
         self.all_contacts = []
@@ -17,7 +18,7 @@ class COVID_Contacts_Information:
         self.master.configure(bg="#370607")
         self.load_contacts_from_file()
         self.menu_window = None
-        
+
         # Background Image
         bg_image = Image.open("C:\\Users\\gerik\\OneDrive\\Documents\\GitHub\\COVID-19-Contact-Tracing\\Main Window Image - Virus.jpg")
         bg_photo = ImageTk.PhotoImage(bg_image)
@@ -33,7 +34,7 @@ class COVID_Contacts_Information:
         rect_width = canvas_width
         rect_height = canvas_height
         self.canvas.create_rectangle(0, 0, rect_width, rect_height, fill='#370607')
-        
+
         # Font Style
         title_font_style = tkfont.Font(family="Poppins", size=50, weight="bold")
         welcome_label_font_style = tkfont.Font(family="Poppins", size=20, weight="bold")
@@ -62,28 +63,23 @@ class COVID_Contacts_Information:
         button_y = (label_y + rect_height) / 3.25
         self.start_button = tk.Button(self.canvas, text="START", font=("Poppins", 30), command=self.on_start_button_click, bg="#006B65", fg="#370607")
         self.start_button.place(x=button_x, y=button_y, anchor=tk.CENTER)
-    
-    def load_contacts_from_file(self): # to load the contacts saved in csv
+
+    def load_contacts_from_file(self):  # to load the contacts saved in csv
         try:
             with open("covid_contacts_record.csv", mode="r") as file:
-                reader = csv.reader(file) # set read mode using csv reader
-                self.all_contacts = list(reader) # save sa all_contacts
+                reader = csv.reader(file)  # set read mode using csv reader
+                self.all_contacts = list(reader)  # save sa all_contacts
                 self.new_contacts = self.all_contacts.copy()
-        except FileNotFoundError: # exception handling if file is not found
+        except FileNotFoundError:  # exception handling if file is not found
             self.save_contacts()
-        except Exception as e: # error message
+        except Exception as e:  # error message
             raise Exception(f"Failed to load contact book: {str(e)}")
-    
+
     def save_contacts(self):
         try:
             with open("covid_contacts_record.csv", mode="w", newline="") as file:
                 writer = csv.writer(file)
-
-                for contact in self.new_contacts:
-                    if len(contact) == 4:
-                        writer.writerow(contact)
-
-                writer.writerows(self.new_contacts)  # Save new contacts
+                writer.writerows(self.all_contacts + self.new_contacts)  # Save all contacts together
         except Exception as e:
             messagebox.showerror("Error", f"Failed to save address book: {str(e)}")
 
@@ -99,7 +95,7 @@ class COVID_Contacts_Information:
         self.edit_button.place(x=900, y=160, anchor=tk.NE)
         self.delete_button = tk.Button(self.menu_window, width=20, text="Delete Contact", background='#370607', fg="white", bd=5, font=("Poppins", 12, "bold"), command=self.delete_contact)
         self.delete_button.place(x=900, y=240, anchor=tk.NE)
-        self.view_button = tk.Button(self.menu_window, width=20, text="View Contacts", background='#370607',fg="white", bd=5, font=("Poppins", 12, "bold"), command=self.view_contacts)
+        self.view_button = tk.Button(self.menu_window, width=20, text="View Contacts", background='#370607', fg="white", bd=5, font=("Poppins", 12, "bold"), command=self.view_contacts)
         self.view_button.place(x=900, y=320, anchor=tk.NE)
         self.search_button = tk.Button(self.menu_window, width=20, text="Search Contact", background='#370607', fg="white", bd=5, font=("Poppins", 12, "bold"), command=self.search_contacts)
         self.search_button.place(x=900, y=400, anchor=tk.NE)
@@ -107,14 +103,13 @@ class COVID_Contacts_Information:
         self.exit_button.place(x=855, y=480, anchor=tk.NE)
 
     def show_menu(self):
-        
+
         if self.menu_window is None or not self.menu_window.winfo_exists():
             # If the menu window doesn't exist or is already closed, create a new one
             self.menu_window = tk.Toplevel(self.master)
             self.menu_window.title("Menu")
             self.menu_window.geometry("1000x600")
             self.menu_window.protocol("WM_DELETE_WINDOW", self.close_menu_window)
-
             self.canvas = tk.Canvas(self.menu_window, width=1000, height=600)
             self.canvas.pack()
 
@@ -126,21 +121,20 @@ class COVID_Contacts_Information:
             bg_image = bg_image.resize((500, 600))
             bg_photo = ImageTk.PhotoImage(bg_image)
 
-           # Create a label with the image
+            # Create a label with the image
             bg_label = tk.Label(self.canvas, image=bg_photo)
             bg_label.image = bg_photo
 
             # Place the label inside the canvas
             self.canvas.create_window(250, 300, window=bg_label)
+            self.create_menu_content()  # Content for menu window
 
-            self.create_menu_content() # Content for menu window
-            
         else:
             # If the menu window is already open, just bring it to the front
             self.menu_window.lift()
 
         self.menu_window.deiconify()
-        
+
     def add_contact(self):
         new_contact_added = False
 
@@ -158,6 +152,8 @@ class COVID_Contacts_Information:
                     messagebox.showinfo("Invalid Input", "Last Name cannot contain numbers.")
                 elif not re.match(r"[^@]+@[^@]+\.[^@]+", address) or "@gmail.com" not in address:
                     messagebox.showinfo("Invalid Input", "Invalid or email address.")
+                elif any(contact[2] == address and contact[3] == int(contact_number) for contact in self.new_contacts):
+                    messagebox.showinfo("Duplicate Entry", "The contact already exists in the address book.")
                 else:
                     try:
                         contact_number = int(contact_number)  # Convert the contact number to an integer
@@ -171,16 +167,13 @@ class COVID_Contacts_Information:
                         self.menu_window.deiconify()
                     except ValueError:
                         messagebox.showinfo("Invalid Input", "Contact number must be a valid number.")
-                    except IndexError:
-                        messagebox.showinfo("Invalid Input", "Index out of range.")
-
             else:
                 messagebox.showinfo("Invalid Input", "All fields are required.")
 
         window = tk.Toplevel(self.menu_window)
         window.title("Add Contact")
         window.geometry("1000x600")
-        window.resizable(False,False)
+        window.resizable(False, False)
 
         # Entry fields
         first_name_label = tk.Label(window, text="First Name:")
@@ -206,19 +199,9 @@ class COVID_Contacts_Information:
         # Save button
         save_button = tk.Button(window, text="Save", command=save_contact)
         save_button.pack()
-        
-        self.menu_window.withdraw()# Hide the menu window while the "Add Contact" window is open
-        
-    def save_contacts(self):
-        try:
-            with open("covid_contacts_record.csv", mode="w", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerows(self.all_contacts + self.new_contacts)  # Save all contacts together
 
-            # After successfully saving the contacts, set new_contact_added to True
-        except Exception as e:
-            messagebox.showerror("Error", f"Failed to save address book: {str(e)}")
-        
+        self.menu_window.withdraw()  # Hide the menu window while the "Add Contact" window is open
+
     def edit_contact(self):
         def edit_selected_contact():
             # Get the selected contact from the listbox
@@ -269,13 +252,12 @@ class COVID_Contacts_Information:
         def cancel_edit():
             edit_window.destroy()
             self.show_menu()
-            
+
         # Create the "Edit Contact Fields" window
         edit_window = tk.Toplevel(self.menu_window)
         edit_window.title("Edit Contact Fields")
         edit_window.geometry("1000x600")
         edit_window.resizable(False, False)
-
         edit_options = ["First Name", "Last Name", "Email Address", "Contact Number"]
         fields = {}
 
@@ -291,12 +273,13 @@ class COVID_Contacts_Information:
         # Save and Cancel buttons
         save_button = tk.Button(edit_window, text="Save", command=lambda: save_edited_contact(contact, fields))
         save_button.pack(pady=10)
+
         cancel_button = tk.Button(edit_window, text="Cancel", command=cancel_edit)
         cancel_button.pack(pady=5)
 
     def delete_contact(self):
         def delete_selected_contact():
-        # Get the selected contact from the listbox
+            # Get the selected contact from the listbox
             selected_index = contact_listbox.curselection()
             if selected_index:
                 selected_contact = self.new_contacts[selected_index[0]]
@@ -308,7 +291,7 @@ class COVID_Contacts_Information:
                 self.menu_window.deiconify()
             else:
                 messagebox.showinfo("Invalid Input", "Please select a contact to delete.")
-        
+
         # Create the "Delete Contact" window
         delete_window = tk.Toplevel(self.menu_window)
         delete_window.title("Delete Contact")
@@ -324,7 +307,7 @@ class COVID_Contacts_Information:
             contact_listbox.insert(tk.END, name)
         contact_listbox.pack(pady=10)
 
-            # Button to select the contact to delete
+        # Button to select the contact to delete
         delete_button = tk.Button(delete_window, text="Delete", command=delete_selected_contact)
         delete_button.pack(pady=5)
 
@@ -348,7 +331,7 @@ class COVID_Contacts_Information:
         tree.heading("Contact Number", text="Contact Number")
 
         # Configure column widths
-        tree.column("#0", width= 100)
+        tree.column("#0", width=100)
         tree.column("First Name", width=100)
         tree.column("Last Name", width=100)
         tree.column("Email Address", width=200)
@@ -363,10 +346,10 @@ class COVID_Contacts_Information:
         def close_view_window():
             view_window.destroy()
             self.show_menu()
-            self.menu_window.deiconify
+            self.menu_window.deiconify()
 
         view_window.protocol("WM_DELETE_WINDOW", close_view_window)
-    
+
     def search_contacts(self):
         self.menu_window.withdraw()  # Hide the menu window while the search window is open
 
@@ -374,6 +357,7 @@ class COVID_Contacts_Information:
         search_window.title("Search Contact")
         search_window.geometry("1000x600")
         search_window.resizable(False, False)
+
         self.search_window = search_window
 
         entry_label = tk.Label(search_window, text="Enter search query:")
@@ -391,7 +375,6 @@ class COVID_Contacts_Information:
         self.search_window.destroy()
         self.menu_window.deiconify()  # Show the menu window again
         self.menu_window.lift()
-        
 
     def perform_search(self, search_criteria, search_query=None, search_window=None):
         if search_query:
@@ -405,7 +388,6 @@ class COVID_Contacts_Information:
                     results.append(contact)
                 elif search_criteria is None or search_criteria == "Contact Number" and search_query.lower() in str(contact[3]):
                     results.append(contact)
-
             if results:
                 self.show_search_results(results, search_window)
             else:
@@ -454,10 +436,9 @@ class COVID_Contacts_Information:
         for i, field in enumerate(["First Name:", "Last Name:", "Address:", "Contact Number:"]):
             label = tk.Label(contact_window, text=field)
             label.grid(row=i, column=0)
-
             value = tk.Label(contact_window, text=contact[i])
             value.grid(row=i, column=1)
-    
+
     def on_exit(self):
         self.save_contacts()
         # Close the entire application by destroying the root window
@@ -466,6 +447,7 @@ class COVID_Contacts_Information:
     # Destroy the menu window and show the main window
     def close_menu_window(self):
         self.menu_window.destroy()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
